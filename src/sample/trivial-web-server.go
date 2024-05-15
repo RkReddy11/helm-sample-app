5e844cb4a3e7fdc3c37a9fdc177313c6cf4d7ef1
Ramakrishna Reddy Lakkireddy <38565567+RkReddy11@users.noreply.github.com>
2024-05-15 17:10:31 +0530

    Update trivial-web-server.go

--- src/sample/trivial-web-server.go
+++ src/sample/trivial-web-server.go
@@ -35,7 +35,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	</head>
 	<body>
 		<div class="container">
-			<span class="message">I am a GO application running inside a container - <span style="color: #ffc107;">v1.1</span>, I am just a Basic Version and I want to Improve massively.</span>
+			<span class="message">I am a GO application running inside a container - <span style="color: #ffc107;">v1.2</span>, Just Now, I leveled up.</span>
 		</div>
 	</body>
 	</html>
